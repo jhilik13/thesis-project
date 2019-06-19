ab90c25cfd8cf05a5c1768055fa6f80cd260f972
jhilik13 <33604352+jhilik13@users.noreply.github.com>
2019-06-20 01:43:20 +0600

    Add files via upload
    functions to resize the image

---
+++ utilsDb.py
@@ -0,0 +1,86 @@
+# Function for resizing image
+
+import cv2
+import numpy as np
+
+def resize(img,rows,cols):
+    '''
+    pass the image and row/col shape
+    returns resized image
+
+    '''
+    return cv2.resize(img, (cols,rows) , interpolation = cv2.INTER_CUBIC)
+
+def resizeStack(stack,plane,size):
+    '''
+    resize a stack of images
+    '''
+    xsize, ysize, zsize = size
+    if plane == 'yz':
+        resized = [resize(slic,ysize,zsize) for slic in stack]
+    if plane == 'zx':
+        resized = [resize(slic,zsize,xsize) for slic in stack]
+    if plane == 'xy':
+        resized = [resize(slic,xsize,ysize) for slic in stack]
+    return resized
+
+
+def capScan(scan,thresh):
+    '''
+    caps the maximum values of the scan to threshold by each pixel
+    '''
+    f = lambda x : thresh if x>thresh else x
+    capped = np.zeros(scan.shape)
+    for i in range(scan.shape[0]):
+        for j in range(scan.shape[1]):
+            for k in range(scan.shape[2]):
+                capped[i][j][k] = f(scan[i][j][k])
+    return capped
+
+
+def normalize(scan,max):
+    '''
+    normalize scan values by max threshold
+    '''
+    return scan/4000.0
+
+def return2DslicesAsList(scan,plane):
+    '''
+    Takes in a 3D Scan and returns slices as a list along
+    axis defined by plane. 
+    plane = 'yz' , 'zx', 'xy'
+    '''
+    slices = []
+    if plane == 'yz':
+        for i in range(35):
+            slices.append(scan[i,:,:])
+    if plane == 'zx':
+        for i in range(scan.shape[1]):
+            slices.append(scan[:,i,:])
+    if plane == 'xy':
+        for i in range(scan.shape[2]):
+            slices.append(scan[:,:,i])
+    return slices
+
+
+
+'''
+
+# unnecesssary garbage
+
+thresh = 4000
+f = lambda x : thresh if x>thresh else x
+
+clipped = map(f,img)
+
+x = list(map(f,singleSlice))
+
+import h5py
+
+h5file = h5py.File("./data/testfile.h5","r")
+
+singleSlice = h5file["image"][...]
+singleMask = h5file["mask"][...]
+
+h5file.close()
+'''
